ca2047c9277bd7eba2159eae23272cdc62609e93
Filip Białek <fp.bialek@gmail.com>
2023-03-22 00:01:01 +0100

    my idea didn't work so I change everything, I learned a lot of new things by which I also added some new buttons :)

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const digits = document.querySelectorAll('.digits')
+const action = document.querySelectorAll('.action')
+const clear = document.querySelectorAll('.clear')
+const deleteOne = document.querySelectorAll('.delete')
+const score = document.querySelectorAll('.score2')
+const prevScore = document.querySelectorAll('.prev-action')
+const currScore = document.querySelectorAll('.curr-action')
+
+let currAction = ''
+let prevAction = ''
+let actionOne = undefined
